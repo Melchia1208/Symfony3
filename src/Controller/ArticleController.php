ee807c0a3f056fd4cdaa1c1fa0640c1c9c60df34
Damien Bournac <damienbournac@gmail.com>
2018-11-20 16:54:09 +0100

    Symfony 10 CRUD

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,49 +3,88 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Entity\Category;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/article")
+ */
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="article")
+     * @Route("/", name="article_index", methods="GET")
      */
-    public function index()
+    public function index(ArticleRepository $articleRepository): Response
     {
+        return $this->render('article/index.html.twig', ['articles' => $articleRepository->findAll()]);
+    }
 
-        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+    /**
+     * @Route("/new", name="article_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
 
-        foreach ($categories as $category) {
-            $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(['category' => $category->getId()]);
-
-            foreach ($articles as $article) {
-                $category->addArticle($article);
-            }
-
-            $tableau[] = $category->getArticles();
+            return $this->redirectToRoute('article_index');
         }
 
-
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-            /*	        'categories' => $categories,
-                        'articles' => $articles,*/
-            'tableau' => $tableau
+        return $this->render('article/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
         ]);
     }
 
+    /**
+     * @Route("/{id}", name="article_show", methods="GET")
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', ['article' => $article]);
+    }
 
     /**
-     * @Route("/article/{id}", name="article_id")
+     * @Route("/{id}/edit", name="article_edit", methods="GET|POST")
      */
-    public function ArticleById($id)
+    public function edit(Request $request, Article $article): Response
     {
-        $article = $this->getDoctrine()->getRepository(Article::class)->findOneBy(['id'=>$id]);
-        //$tags = $article->getTags();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
 
-        return $this->render('article/articlebyid.html.twig', ['article' => $article]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_edit', ['id' => $article->getId()]);
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="article_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($article);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('article_index');
     }
 }
